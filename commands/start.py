222a96c542a1702d5a37508dd321a7cbc21377c5
1rayofhope1 <sergey11091998@gmail.com>
2023-11-25 04:30:27 +0700

    commands

--- commands/start.py
+++ commands/start.py
@@ -1,5 +1,13 @@
 from aiogram import types
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButtonPollType
 
 
 async def start(message: types.Message) -> None:
-    await message.answer('Привет')
+    menu_bilder = ReplyKeyboardBuilder()
+    menu_bilder.button(text='Помощь')
+    menu_bilder.add(types.KeyboardButton(
+        text='Отправить контакт', request_contact=True))
+    menu_bilder.row(types.KeyboardButton(
+        text='Отправить голосование', request_poll=KeyboardButtonPollType()))
+
+    await message.answer('Привет', reply_markup=menu_bilder.as_markup())
